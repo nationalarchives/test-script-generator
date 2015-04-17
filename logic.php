c20b2ab9ac77b754f78d65f3f207a9b4005901b2
Gwyn Jones <gjones@nationalarchives.gsi.gov.uk>
2015-04-17 19:17:07 +0100

    PHPStorm reformat

--- logic.php
+++ logic.php
@@ -1,122 +1,125 @@
-<?php 
+<?php
 
 /**
- * 	Prints a heading showing a team member and the browser that they will be testing.
- *	
- *	@param 		string 	$teamMember 	Team members name
- *	@param 		string 	$browser 		Browser name
+ *    Prints a heading showing a team member and the browser that they will be testing.
  *
-*/
-function printHeading($teamMember, $browser) {
+ * @param   string $teamMember  Team members name
+ * @param   string $browser     Browser name
+ *
+ */
+function printHeading($teamMember, $browser)
+{
 
-	$headingFormat = "<li class='list-group-item active'><h2>%s <small><span class='label label-default'>%s</span></small></h2></li>";
+    $headingFormat = "<li class='list-group-item active'><h2>%s <small><span class='label label-default'>%s</span></small></h2></li>";
 
-	printf($headingFormat, $teamMember, $browser);
+    printf($headingFormat, $teamMember, $browser);
 
 }
 
 
 /**
- * 	Prints a specified number of pages to be tested 
- *	
- *	@param 		int 	$numberOfPagesPerTester 	The number of pages that each individual tester should test
- *	@param 		array 	$pages 						Array representing all pages available for testing (NOTE: This is passed by reference)
- *  @param 		array 	$pagesAlwaysTested			Array representing those high profile pages which are always tested
- * 	@var 		int 	$numberOfAlwaysTestedPages 	The number of pages that are always tested. This is used in the loop and deducted from 
- *													the number of pages that each individual tester should test
+ *    Prints a specified number of pages to be tested
  *
-*/
-function printPagesToTest($numberOfPagesPerTester, &$pages, &$pagesAlwaysTested) {
+ * @param   int     $numberOfPagesPerTester The number of pages that each individual tester should test
+ * @param   array   $pages Array representing all pages available for testing (NOTE: This is passed by reference)
+ * @param   array   $pagesAlwaysTested Array representing those high profile pages which are always tested
+ * @var     int     $numberOfAlwaysTestedPages The number of pages that are always tested. This is used in the loop and deducted from
+ *                  the number of pages that each individual tester should test
+ *
+ */
+function printPagesToTest($numberOfPagesPerTester, &$pages, &$pagesAlwaysTested)
+{
 
-	$k = 1; 
+    $k = 1;
 
-	$items = array();
+    $items = array();
 
-	$numberOfAlwaysTestedPages = 3;
-	
-	// Everyone gets a couple of $pagesAlwaysTested 
+    $numberOfAlwaysTestedPages = 3;
 
-	for ($i=0; $i < $numberOfAlwaysTestedPages; $i++) { 
+    // Everyone gets a couple of $pagesAlwaysTested
 
-		$pageAlwaysTested = array_pop($pagesAlwaysTested);
+    for ($i = 0; $i < $numberOfAlwaysTestedPages; $i++) {
 
-		if($pageAlwaysTested !== NULL) {
+        $pageAlwaysTested = array_pop($pagesAlwaysTested);
 
-			$liFormat = "<li class='list-group-item'><span class='label label-warning'>Page always tested</span> %s <div class='btn-group'><a href='%s' class='btn btn-default' target='_blank'>Test</a> <a href='%s' class='btn btn-default' target='_blank'>Live</a></div></li>";
-			
-			$items[] = sprintf($liFormat, str_replace('http://testlb.nationalarchives.gov.uk', '', $pageAlwaysTested), $pageAlwaysTested, str_replace('testlb', 'livelb', $pageAlwaysTested));
+        if ($pageAlwaysTested !== NULL) {
 
-		}
-	}
+            $liFormat = "<li class='list-group-item'><span class='label label-warning'>Page always tested</span> %s <div class='btn-group'><a href='%s' class='btn btn-default' target='_blank'>Test</a> <a href='%s' class='btn btn-default' target='_blank'>Live</a></div></li>";
+
+            $items[] = sprintf($liFormat, str_replace('http://testlb.nationalarchives.gov.uk', '', $pageAlwaysTested), $pageAlwaysTested, str_replace('testlb', 'livelb', $pageAlwaysTested));
+
+        }
+    }
 
 
-	// Selection of random pages
-	while ($k <= ($numberOfPagesPerTester - $numberOfAlwaysTestedPages)) {
+    // Selection of random pages
+    while ($k <= ($numberOfPagesPerTester - $numberOfAlwaysTestedPages)) {
 
-		$page = array_pop($pages); 
+        $page = array_pop($pages);
 
-		if (pageShouldBeTested($page)) {
-	
-			$liFormat = "<li class='list-group-item'>%s <div class='btn-group'><a href='%s' class='btn btn-default' target='_blank'>Test</a> <a href='%s' class='btn btn-default' target='_blank'>Live</a></div></li>";
+        if (pageShouldBeTested($page)) {
 
-			$items[] = sprintf($liFormat, str_replace('http://testlb.nationalarchives.gov.uk', '', $page), $page, str_replace('testlb', 'livelb', $page));
+            $liFormat = "<li class='list-group-item'>%s <div class='btn-group'><a href='%s' class='btn btn-default' target='_blank'>Test</a> <a href='%s' class='btn btn-default' target='_blank'>Live</a></div></li>";
 
-			$k++;
-		}
-	}
+            $items[] = sprintf($liFormat, str_replace('http://testlb.nationalarchives.gov.uk', '', $page), $page, str_replace('testlb', 'livelb', $page));
 
-	printf("<ul class='list-group'>%s</ul>", implode($items));
+            $k++;
+        }
+    }
+
+    printf("<ul class='list-group'>%s</ul>", implode($items));
 
 }
 
 /**
- * 	Determines if a specific page should be excluded from the testing based on it containing a specific sub-string.
- *	This has been included to prevent certain page types being over-represented in testing (for example, while almost 2/3 of all
- *	pages are education resources, all of these use the same template and styles and they should not therefore be over-represented)
- *	
- *	@param 		string 	$candidateHaystack 	String to search
+ *    Determines if a specific page should be excluded from the testing based on it containing a specific sub-string.
+ *    This has been included to prevent certain page types being over-represented in testing (for example, while almost 2/3 of all
+ *    pages are education resources, all of these use the same template and styles and they should not therefore be over-represented)
  *
- *	@var 		array 	$overRepresentedPages 	Array of strings which, if found in the tested URL, mean is likely to be over-represented in results. 
- *											For this reason, only 20% of such pages will considered OK to include (using rand())
+ * @param   string $candidateHaystack   String to search
  *
- *	@return 	bool 						True indicates the page can be included in testing, False that it should be excluded. 
-*/
+ * @var     array $overRepresentedPages Array of strings which, if found in the tested URL, mean is likely to be over-represented in results.
+ *                                      For this reason, only 20% of such pages will considered OK to include (using rand())
+ *
+ * @return  bool                        True indicates the page can be included in testing, False that it should be excluded.
+ */
 
-function pageShouldBeTested($candidateHaystack) {
+function pageShouldBeTested($candidateHaystack)
+{
+    $overRepresentedPages = array('beta', 'resources');
 
-	$overRepresentedPages = array('beta', 'resources');
+    $proceed = true;
 
-	$proceed = true; 
+    foreach ($overRepresentedPages as $value) {
 
-	foreach ($overRepresentedPages as $value) {
+        if (strpos($candidateHaystack, $value)) {
 
-		if (strpos($candidateHaystack, $value)) {
+            $proceed = false;
 
-			$proceed = false; 
+        }
+    }
 
-		} 
-	}
-
-	return $proceed;
+    return $proceed;
 
 }
 
 /**
- * 	Generates the full list of testers and the pages they'll be testing 
- *	
- *	@param 		array 	$testers 	Associative array of testers. Keys are tester name, values are browser name
- * 
- *	@param 		array 	$pages 		Array representing all pages available for testing
+ *    Generates the full list of testers and the pages they'll be testing
  *
-*/
+ * @param   array   $testers    Associative array of testers. Keys are tester name, values are browser name
+ *
+ * @param   array   $pages      Array representing all pages available for testing
+ *
+ */
 
-function generateTestScript($testers, $pages, $pagesAlwaysTested) {
+function generateTestScript($testers, $pages, $pagesAlwaysTested)
+{
 
-	foreach ($testers as $key => $value) {
+    foreach ($testers as $key => $value) {
 
-		printHeading($key, $value);
+        printHeading($key, $value);
 
-		printPagesToTest(20, $pages, $pagesAlwaysTested);
-		
-	}
+        printPagesToTest(20, $pages, $pagesAlwaysTested);
+
+    }
 }
